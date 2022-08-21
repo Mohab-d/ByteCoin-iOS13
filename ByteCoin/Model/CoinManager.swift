fc9ca3d0ae0a1ce4e67d04c2abb13ef7857ad8d9
Mohammad Mohab <mohabuisness.dev@gmail.com>
2022-08-21 15:47:31 +0200

    setup the pickerView delegate and data source and fill in the data in  ViewController. create the data url in CoinManager.

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -11,9 +11,14 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "9B2EF526-1068-460E-872B-40D7EA3DDE36"
+
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     
+    func makeURL(currency: String) -> String{
+        let url = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        return url
+    }
 }
